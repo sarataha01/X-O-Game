7de351ae1bd4e91ea474d3a9b917ef49c0b11e70
sarataha01 <101889904+sarataha01@users.noreply.github.com>
2022-08-04 01:38:14 +0200

    Update Source.c

--- Source.c
+++ Source.c
@@ -11,36 +11,21 @@ X-O TASK 1
 #include <stdbool.h>
 #pragma warning(disable : 4996)             //disable warnings for scanf
 
-char arr[3][3] = { {' ',' ',' '},{' ',' ',' '},{' ',' ',' '} };  //initialize XO table with spaces
-char ans;                                                       //X-O input
+char matrix[3][3] = { {' ',' ',' '},{' ',' ',' '},{' ',' ',' '} };  //initialize XO table with spaces
+char choice;                                                       //X-O input
 
 
-bool input(int row,int column)
+bool input(int column, int row)
 {
-    if (arr[column - 1][row - 1] == ' ')   // -1 because array starts from 0 not 1
+    if (matrix[row - 1][column - 1] == ' ')   // -1 because array starts from 0 not 1
     {
-        arr[column - 1][row - 1] = ans;
+        matrix[row - 1][column - 1] = choice;
         return true;                    // x or o added
     }
     else
         return false;                   //if place is full or doesn't exist
 }
 
-void Print()
-{
-    int i;
-
-    for (i = 0; i < 3; i++) 
-    {   
-        printf(" %c | %c | %c ", arr[i][0], arr[i][1], arr[i][2]);
-
-        if (i != 2) 
-        {
-            printf("\n---|---|---\n");
-        }
-    }
-    printf("\n\n");
-}
 
 char WinMove(char arr[3][3])          //checks all possible winning rows, columns,and diagonals
 {
@@ -112,23 +97,39 @@ char WinMove(char arr[3][3])          //checks all possible winning rows, column
         return 't';             //if no winning move is found, then it's a tie
 }
 
+void display()
+{
+    int i;
+
+    for (i = 0; i < 3; i++)
+    {
+        printf(" %c | %c | %c ", matrix[i][0], matrix[i][1], matrix[i][2]);
+
+        if (i != 2)
+        {
+            printf("\n---|---|---\n");
+        }
+    }
+    printf("\n\n");
+}
+
 int main()
 {
     do
     {
         printf("Enter x for Player 1 or o for Player 2: ");
 
-        scanf(" %c", &ans);
-    } while (ans != 'x' && ans != 'o');
+        scanf(" %c", &choice);
+    } while (choice != 'x' && choice != 'o');
 
     int column, row;
-    ans = 'x';
+    //ans = 'x';
 
     printf("   1  2  3 \n 1\n 2\n 3\n");        //coordinates for table
 
     for (int i = 0; i < 9; i++)
     {
-        printf("\n%c turn\nEnter coordinates: ", ans);
+        printf("\n%c turn\nEnter coordinates: ", choice);
         printf("\nThe column: ");
         scanf("%d", &column);
         printf("The row: ");
@@ -137,31 +138,31 @@ int main()
 
         if (input(column, row))
         {
-            Print();
-            if (WinMove(arr) == 'x')
+            display();
+            if (WinMove(matrix) == 'x')
             {
                 printf("x WINS!");
                 break;
             }
-            else if (WinMove(arr) == 'o')
+            else if (WinMove(matrix) == 'o')
             {
                 printf("o WINS!");
                 break;
             }
-            else if ((WinMove(arr) == 't') && (i == 8))
+            else if ((WinMove(matrix) == 't') && (i == 8))
             {
                 printf("Game Over - TIE!\n");
             }
 
-            if (ans == 'x')             //to change turns bet. X & O
-                ans = 'o';
+            if (choice == 'x')             //to change turns bet. X & O
+                choice = 'o';
             else
-                ans = 'x';
+                choice = 'x';
         }
         else
         {
             i--;                        //to not count as a turn
-            printf("Cell wasn't empty. Try again\n");
+            printf("olace is not empty. Try again\n");
         }
     }
     return 0;
